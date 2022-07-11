a726afae47c5dbc85e897499876627089092f1d1
nour az <naz.199860@gmail.com>
2022-07-11 04:21:56 +0300

    get user posts

--- Backend/src/user/controller.js
+++ Backend/src/user/controller.js
@@ -2,3 +2,18 @@ const { getUsers, getById, addUser, getByEmail}= require('./service');
 const bcrypt= require('bcryptjs');
 const TOKEN_SECRET = process.env.TOKEN_SECRET || "";
 const jwt=require('jsonwebtoken');
+
+async function get(req, res){
+    try{
+        if(req.query.id)
+        {
+            const id = req.query.id;
+            const result= await getById(id);
+            return res.send(result);
+        }
+        const result=getUsers();
+        return res.send(result);
+    }catch(error){
+        console.log(error);
+    }
+}
