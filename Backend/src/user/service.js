1a22c2524c5dbfa11e8720e6cdfa0efa8e51e423
nour az <naz.199860@gmail.com>
2022-07-11 03:18:08 +0300

    get user by id

--- Backend/src/user/service.js
+++ Backend/src/user/service.js
@@ -3,3 +3,6 @@ const User = require('../../model/User');
 async function getUsers(){
     return await User.find();
 }
+async function getById(id) {
+    return await User.findById(id).populate('contacts');
+  }
