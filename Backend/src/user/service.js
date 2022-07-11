66f86e18b122db1b10fd46a27eaf352c9df22e45
nour az <naz.199860@gmail.com>
2022-07-11 03:37:15 +0300

    get user by email

--- Backend/src/user/service.js
+++ Backend/src/user/service.js
@@ -10,13 +10,27 @@ async function getById(id) {
     const {
       name,
       email,
+      phone,
     } = body;
   
     const user = new User({
       name,
       email,
+      phone,
       password: hashPassword
     });
   
     return await user.save();
+  }
+
+  async function getByEmail(email) {
+    return await User.findOne({
+      email
+    });
+  }
+  module.exports={
+    getUsers,
+    addUser,
+    getById,
+    getByEmail,
   }
