b2576ed4f13a65b067d7143c313c70955cc2b133
nour az <naz.199860@gmail.com>
2022-07-11 03:21:46 +0300

    add user

--- Backend/src/user/service.js
+++ Backend/src/user/service.js
@@ -6,3 +6,17 @@ async function getUsers(){
 async function getById(id) {
     return await User.findById(id).populate('contacts');
   }
+  async function addUser(body, hashPassword) {
+    const {
+      name,
+      email,
+    } = body;
+  
+    const user = new User({
+      name,
+      email,
+      password: hashPassword
+    });
+  
+    return await user.save();
+  }
