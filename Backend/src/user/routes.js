520f4017bcf4de4e75dec37dcbad6ca0b05f08ff
nour az <naz.199860@gmail.com>
2022-07-11 06:50:49 +0300

    user toutes and creating middleware file

---
+++ Backend/src/user/routes.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const { get, register, login } = require('./controller');
+const router = Router();
+
+router.post('register', register);
+router.post('/login', login);
+module.exports = router;
+
