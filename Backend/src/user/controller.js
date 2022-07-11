d8666653c65057c20bd514151dba3ad970c0205c
nour az <naz.199860@gmail.com>
2022-07-11 04:37:40 +0300

    register function

--- Backend/src/user/controller.js
+++ Backend/src/user/controller.js
@@ -1,19 +1,28 @@
-const { getUsers, getById, addUser, getByEmail}= require('./service');
-const bcrypt= require('bcryptjs');
+const { getUsers, getById, addUser, getByEmail } = require('./service');
+const bcrypt = require('bcryptjs');
 const TOKEN_SECRET = process.env.TOKEN_SECRET || "";
-const jwt=require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-async function get(req, res){
-    try{
-        if(req.query.id)
-        {
+async function get(req, res) {
+    try {
+        if (req.query.id) {
             const id = req.query.id;
-            const result= await getById(id);
+            const result = await getById(id);
             return res.send(result);
         }
-        const result=getUsers();
+        const result = getUsers();
         return res.send(result);
-    }catch(error){
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function register(req, res) {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(req.body.password, salt);
+        const addUserResult = await addUser(req.body, hashPassword);
+        return res.send({ user: addUserResult._id });
+    } catch (error) {
         console.log(error);
     }
 }
