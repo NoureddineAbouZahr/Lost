441d65ee0c100cb296432e4e5c167d5fdc71244d
nour az <naz.199860@gmail.com>
2022-07-11 16:23:54 +0300

    get sub categories

--- Backend/src/sub/controller.js
+++ Backend/src/sub/controller.js
@@ -1,23 +1,35 @@
-const {addSub,getSubs}=require("./service");
-const Sub=require("../../model/Sub");
-const Category=require("../../model/Category");
+const { addSub, getSubs } = require("./service");
+const Sub = require("../../model/Sub");
+const Category = require("../../model/Category");
 
-async function add(req,res){
-    try{
-        const newSub=await addSub(req.body);
-        const updateCategory=await Category.updateMany(
+async function add(req, res) {
+    try {
+        const newSub = await addSub(req.body);
+        const updateCategory = await Category.updateMany(
             {
-                _id:newSub.cid
+                _id: newSub.cid
             },
             {
-                $push:{
-                    Subs:newSub._id
+                $push: {
+                    Subs: newSub._id
                 }
             }
         );
         return res.status(200).send(newSub);
-    }catch(error){
+    } catch (error) {
         console.log(error);
         res.status(500).send(error);
     }
 }
+async function get(req, res) {
+    try {
+        const result = await getSubs(req.query.id);
+        return res.send(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
+module.exports = {
+    add,
+    get,
+};
