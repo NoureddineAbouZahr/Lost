f31cb15b3d3470a67f32bbbb96adcc2227483321
nour az <naz.199860@gmail.com>
2022-07-13 00:35:11 +0300

    add categories in service

--- Backend/src/category/service.js
+++ Backend/src/category/service.js
@@ -1,5 +1,16 @@
 const Category = require('../../model/Category');
 
-async function getCats(){
+async function getCats() {
     return await Category.find();
+}
+
+
+async function addCategory(body) {
+    const {
+        name,
+    } = body;
+    const cat = new Category({
+        name,
+    });
+    return await cat.save();
 }
