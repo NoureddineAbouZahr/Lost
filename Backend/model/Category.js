5a1272453ec800195af6528b702e4fdf637a6de1
nour az <naz.199860@gmail.com>
2022-07-13 00:21:24 +0300

    fixing category model

--- Backend/model/Category.js
+++ Backend/model/Category.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose');
 
-const subCategoriesSchema = new mongoose.Schema({
+/*const subCategoriesSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
     }
-});
+});*/
 
 const categorySchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
     },
-    subCategories: [subCategoriesSchema],
+    //subCategories: [subCategoriesSchema],
+    subid:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Sub'
+    },
 });
 
 module.exports = mongoose.model('Category', categorySchema);
