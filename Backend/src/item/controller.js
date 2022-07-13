584bf1cffdf628db24c5c9e0f6636e4fb3609376
nour az <naz.199860@gmail.com>
2022-07-13 09:02:58 +0300

    add item api

--- Backend/src/item/controller.js
+++ Backend/src/item/controller.js
@@ -1,3 +1,23 @@
-const {addItem, getItems, getItemsbyUser}=require('./service');
-const Sub=require('../../model/Sub');
-const Item=require('../../model/Item');
+const { addItem, getItems, getItemsbyUser } = require('./service');
+const Sub = require('../../model/Sub');
+const Item = require('../../model/Item');
+
+async function add(req, res) {
+    try {
+        const newItem = await addItem(req.body);
+        const updateSub = await Sub.updateMany(
+            {
+                _id: newItem.subs
+            },
+            {
+                $push: {
+                    items: newItem._id
+                }
+            }
+        );
+        return res.status(200).send(newItem);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+}
