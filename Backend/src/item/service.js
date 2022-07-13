51757d171177fe59451cf7140213ab06612ceba9
nour az <naz.199860@gmail.com>
2022-07-13 08:43:56 +0300

    add item (item service)

---
+++ Backend/src/item/service.js
@@ -0,0 +1,33 @@
+const Item = require('../../model/Item');
+
+async function addItem(body) {
+    const {
+        name,
+        SerialNumber,
+        model,
+        color,
+        pic,
+        brand,
+        extraInfo,
+        locationx,
+        locationy,
+        status,
+        sub,
+        user,
+    } = body;
+    const item = new Item({
+        name,
+        SerialNumber,
+        model,
+        color,
+        pic,
+        brand,
+        extraInfo,
+        locationx,
+        locationy,
+        status,
+        sub,
+        user,
+    });
+    return await item.save();
+}
