2f72089ae1d1016c5353592bfcaa80de811c7702
nour az <naz.199860@gmail.com>
2022-07-13 16:50:06 +0300

    item routes

--- Backend/src/item/routes.js
+++ Backend/src/item/routes.js
@@ -1,3 +1,10 @@
 const { Router } = require('express');
 const { add, get, removeItem, getbyUser } = require('./controller');
 const router = Router();
+
+router.post('/addItem', add);
+router.get('/getItems', get);
+router.get('/userItems', getbyUser);
+router.delete('/removeItem', removeItem);
+
+module.exports = router;
