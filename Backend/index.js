92a921e272723cbe9651213c32c73e44918f54f5
nour az <naz.199860@gmail.com>
2022-07-15 16:24:43 +0300

    setting up the connection to database

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,18 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose=require("mongoose");
-const corrs= require("cors");
+const mongoose = require("mongoose");
+const corrs = require("cors");
 
-const userRouter=require("./src/user/routes");
-const subRouter=require("./src/sub/routes");
-const catRouter=require("./src/category/routes");
-const itemRouter=require("./src/item/routes");
+const userRouter = require("./src/user/routes");
+const subRouter = require("./src/sub/routes");
+const catRouter = require("./src/category/routes");
+const itemRouter = require("./src/item/routes");
+
+const DB_CONNECT = process.env.DB_CONNECT || "";
+mongoose.connect(
+    DB_CONNECT,
+    () => console.log("connected")
+);
+const app = express();
+app.use(express.json());
+app.use(cors());
