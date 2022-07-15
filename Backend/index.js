3ba2118aa30820c8077942775a48a976a7cc7a20
nour az <naz.199860@gmail.com>
2022-07-15 16:20:22 +0300

    calling routes

--- Backend/index.js
+++ Backend/index.js
@@ -2,3 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const mongoose=require("mongoose");
 const corrs= require("cors");
+
+const userRouter=require("./src/user/routes");
+const subRouter=require("./src/sub/routes");
+const catRouter=require("./src/category/routes");
+const itemRouter=require("./src/item/routes");
