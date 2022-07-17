4fbe5221b8ec34021b4a132763df8ae4b15773c8
nour az <naz.199860@gmail.com>
2022-07-18 01:51:59 +0300

    getting data and empty inputs on submitting

--- AdminFrontend/admin/src/component/login.js
+++ AdminFrontend/admin/src/component/login.js
@@ -6,45 +6,57 @@ import React from 'react'
 
 const Login = () => {
 
-    const [email,setEmail]=useState("");
-    const [password,setPassword]=useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const e=document.getElementById('email');
+    const p=document.getElementById('password');
 
-    const onSubmit=(e)=>{
-        if(!email || !password){
-            alert("no entered data")
+    const onSubmit = (e) => {
+        if (!email || !password) {
+            alert("no entered data");
             return
         }
-       
+        e.preventDefault();
+
+        let data={
+            email:email,
+            password:password
+        }
+        setEmail('');
+        setPassword('');
+        e.value='';
+        p.value='';
+
     }
 
     return (
         <div className="loginBody">
             <div className="logo">
             </div>
-                <img src={logo} alt="logo" className="lgo"/>
-        <form action="">
+            <img src={logo} alt="logo" className="lgo" />
+            <form action="">
 
-            <div className="form-inner">
-                
-                <div className='hc'>
+                <div className="form-inner">
+
+                    <div className='hc'>
+                    </div>
+                    {/* Error! */}
+                    <div className="form-control">
+                        <label htmlFor="email">Email: </label>
+                        <input type="text" name='email' id='email'
+                            placeholder='Email'
+                            onChange={e => setEmail(e.target.value)} />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="password">Password: </label>
+                        <input type="password" name='password' id='password'
+                            placeholder='Password'
+                            onChange={e => setPassword(e.target.value)} />
+                    </div>
+                    <input type="submit" value=" Log In" className="btn btn-block" />
                 </div>
-                {/* Error! */}
-                <div className="form-control">
-                    <label htmlFor="email">Email: </label>
-                    <input type="text" name='email' id='email'
-                        placeholder='Email'
-                        onChange={e => setEmail(e.target.value)} />
-                </div>
-                <div className="form-control">
-                    <label htmlFor="password">Password: </label>
-                    <input type="password" name='password' id='password'
-                        placeholder='Password'
-                        onChange={e => setPassword(e.target.value)} />
-                </div>
-                <input type="submit" value=" Log In" className="btn btn-block" />
-            </div>
-        </form>
-                        </div>
+            </form>
+        </div>
 
     )
 }
