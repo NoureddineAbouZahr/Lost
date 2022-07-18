5dfe45e0b985690b2967335982bd5c87fa7377a9
nour az <naz.199860@gmail.com>
2022-07-18 13:46:46 +0300

    creating states for the items component

--- AdminFrontend/admin/src/component/Items.js
+++ AdminFrontend/admin/src/component/Items.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import{Map,Marker} from 'pigeon-maps';
-import {useNavigate} from "react-router-dom";
+import { Map, Marker } from 'pigeon-maps';
+import { useNavigate } from "react-router-dom";
 import { Link } from 'react-router-dom';
 
 
 const Items = () => {
-const navigate=useNavigate();
+    const navigate = useNavigate();
+    const [item, setItem] = useState([]);
+    const [center, setCenter] = useState([33.5571, 35.3729]);
+    const [zoom, setZoom] = useState(11);
+    const [search, setSearch] = useState('');
 
-  return (
-    <div className='items'>Items</div>
-  )
+
+    return (
+        <div className='items'>Items</div>
+    )
 }
 
 export default Items
