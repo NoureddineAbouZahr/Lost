ab17e6f4d6eaf2f3b7ebb318b5232c3e8f092309
nour az <naz.199860@gmail.com>
2022-07-18 13:44:55 +0300

    installing pigeon maps and importing needed header

--- AdminFrontend/admin/src/component/Items.js
+++ AdminFrontend/admin/src/component/Items.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import{Map,Marker} from 'pigeon-maps';
+import {useNavigate} from "react-router-dom";
+import { Link } from 'react-router-dom';
+
 
 const Items = () => {
+const navigate=useNavigate();
+
   return (
     <div className='items'>Items</div>
   )
