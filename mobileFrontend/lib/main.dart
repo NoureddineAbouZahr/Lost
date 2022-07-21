0e09a4b315ef305efb088a38cbf7d5d9a213ee16
nour az <naz.199860@gmail.com>
2022-07-22 02:39:49 +0300

    startin with welcome page and adding logo

--- mobileFrontend/lib/main.dart
+++ mobileFrontend/lib/main.dart
@@ -1,7 +1,42 @@
 import 'package:flutter/material.dart';
 
+bool isLogin = false;
 void main() {
-  runApp();
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      // home: isLogin ? Home(logindata) : Login(),
+      home: Welcome(),
+    );
+  }
+}
+
+class Welcome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text('Welcome'),
+          CircleAvatar(
+            backgroundImage: const AssetImage('assets/logo.png'),
+            radius: MediaQuery.of(context).size.width / 2.5,
+            backgroundColor: Colors.transparent,
+          ),
+        ],
+      ),
+    );
+  }
+}
