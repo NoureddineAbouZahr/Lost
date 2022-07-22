eadedb110123c01c1f0ebbf4ea2e9da7051d66db
nour az <naz.199860@gmail.com>
2022-07-22 12:56:00 +0300

    login headers

---
+++ mobileFrontend/lib/pages/login.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import '../widgets/input.dart';
+import '../widgets/lost_button.dart';
+import '../widgets/hyperlink.dart';
+
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    }
+}
