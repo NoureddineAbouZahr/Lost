07c78bfb249c322a3efa438afc78305d2b1e5efd
nour az <naz.199860@gmail.com>
2022-07-22 05:26:46 +0300

    creating splash screen logo display

--- mobileFrontend/lib/splash.dart
+++ mobileFrontend/lib/splash.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
+
 class Splash extends StatelessWidget {
   const Splash({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        
-      ),
+      body:Center(
+      child: Container(
+        child:
+        CircleAvatar(
+          backgroundImage: const AssetImage('assets/logo.png'),
+          radius: width / 2.5,
+          backgroundColor: Colors.transparent,
+        ),
+      ),),
     );
   }
 }
