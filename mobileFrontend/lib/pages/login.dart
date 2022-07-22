a93b35c467b55f8ae55419180ef51040695e8217
nour az <naz.199860@gmail.com>
2022-07-22 12:57:09 +0300

    login app bar

--- mobileFrontend/lib/pages/login.dart
+++ mobileFrontend/lib/pages/login.dart
@@ -9,6 +9,23 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-    }
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: AppBar(
+          centerTitle: true,
+          title: Column(
+            children: const [
+              SizedBox(height: 5),
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/logo.png'),
+                radius: 40,
+                backgroundColor: Colors.transparent,
+              )
+            ],
+          ),
+        ),
+      ),
+     
+    );
+  }
 }
