611baaae787ccee71726e3c5e0008944c7728aa2
nour az <naz.199860@gmail.com>
2022-07-25 02:21:24 +0300

    navbar design and list tiles

--- mobileFrontend/lib/navbar.dart
+++ mobileFrontend/lib/navbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class Nav extends StatefulWidget {
   const Nav({Key? key}) : super(key: key);
 
@@ -9,16 +10,38 @@ class Nav extends StatefulWidget {
 class _NavState extends State<Nav> {
   @override
   Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(children: [
 
-    return
+        Container(
+          decoration:   BoxDecoration(
+            color: Color(0xffefd16f),
+            boxShadow: [BoxShadow(blurRadius: 4)]
+          ),
+          padding: EdgeInsets.all(25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text('Nour', style: TextStyle(fontWeight: FontWeight.bold)),
+              Text('nour@gmail.com')
+            ],
+          ),
+        ),
 
-    Drawer(
-    child: ListView(
-      children: const [
 
-      ],
-    ),
+
+        ListTile(
+          leading: Icon(Icons.my_library_books_rounded),
+          title: Text('Posts'),
+          onTap: () => null,
+        ),
+        ListTile(
+          leading: Icon(Icons.chat),
+          title: Text('Chats'),
+          onTap: () => null,
+        ),
+      ]
+      ),
     );
-
   }
 }
