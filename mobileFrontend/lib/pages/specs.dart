e0bc7788d71a7f8fba0c3007dad54d83293e25d6
nour az <naz.199860@gmail.com>
2022-07-26 01:38:05 +0300

    get image function

--- mobileFrontend/lib/pages/specs.dart
+++ mobileFrontend/lib/pages/specs.dart
@@ -1,15 +1,23 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
-import '../widgets/navbar.dart';
-
-class SpecFound extends StatelessWidget {
+class SpecFound extends StatefulWidget {
   const SpecFound({Key? key}) : super(key: key);
 
+  @override
+  State<SpecFound> createState() => _SpecFoundState();
+}
+
+class _SpecFoundState extends State<SpecFound> {
+  File? image;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60),
+        preferredSize: const Size.fromHeight(60),
         child: AppBar(
           centerTitle: true,
           title: Column(
@@ -27,15 +35,33 @@ class SpecFound extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const SizedBox(
+          children:  [
+            SizedBox(
               height: 50,
             ),
+            Column(
+              children:  [
+                
+                SizedBox(height: 20,),
 
+
+              ],
+            )
 
           ],
         ),
       ),
     );
+  }
+
+  Future pickImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if(image == null) return;
+      final imageTemp = File(image.path);
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch(e) {
+      print('Failed to pick image: $e');
+    }
   }
 }
