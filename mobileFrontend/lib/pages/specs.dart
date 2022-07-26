e0229149133871d76132834b819fd33ff634eefb
nour az <naz.199860@gmail.com>
2022-07-26 14:31:15 +0300

    map

--- mobileFrontend/lib/pages/specs.dart
+++ mobileFrontend/lib/pages/specs.dart
@@ -2,8 +2,11 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_map/flutter_map.dart';
 import 'package:image_picker/image_picker.dart';
+// import 'package:latlong/latlong.dart';
 import 'package:lost/widgets/input.dart';
+import 'package:latlong2/latlong.dart';
 
 class SpecFound extends StatefulWidget {
   const SpecFound({Key? key}) : super(key: key);
@@ -22,6 +25,8 @@ class _SpecFoundState extends State<SpecFound> {
   TextEditingController locationx = new TextEditingController();
   TextEditingController locationy = new TextEditingController();
   TextEditingController status = new TextEditingController();
+
+  LatLng point = LatLng(33, 37);
 
   File? image;
 
@@ -44,86 +49,115 @@ class _SpecFoundState extends State<SpecFound> {
           ),
         ),
       ),
-      body: ListView(
-        children: [Column(
-
+      body: ListView(children: [
+        Column(
           mainAxisAlignment: MainAxisAlignment.start,
-          children:  [
+          children: [
             SizedBox(
               height: 30,
             ),
             Column(
-              children:  [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children:[
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 20),
-
-                  padding: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-
-                      border: Border.all(
-                        color: Colors.grey,
-
-                      ),
-                    borderRadius: BorderRadius.circular(5)
+              children: [
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20),
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(5)),
+                    child: SizedBox(
+                      height: 150,
+                      width: 150,
+                      child:
+                          image != null ? Image.file(image!) : Text('no image'),
+                    ),
                   ),
-                  child:
-                  SizedBox(
-                    height: 150,
-                    width: 150,
-                    child: image !=null ? Image.file(image!):Text('no image'),
-                  ),),
-                MaterialButton(
-                    color: Colors.grey,
-                    shape: RoundedRectangleBorder(
-                      side:const BorderSide(width: 2,color: Color(0xffefd16f)),
-                      borderRadius: BorderRadius.circular(5)
-                    ),
-                    child: const Text(
-                        "Pick Image",
-                        style: TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold,fontSize: 10
-                        )
-                    ),
-                    onPressed:() {
-                      pickImage();
-
-                    }
-                ),]),
-                SizedBox(height: 20,),
-                Container(
-
-                  child:Column(
-                    children:[
-                LostInput(labelText: 'Type', hintText: "Enter  Name ", controller: name),
-                LostInput(labelText: 'Serial Number', hintText: 'Enter  S.N', controller: SerialNumber),
-                LostInput(labelText: 'Model', hintText: 'Enter  Model', controller: model),
-                LostInput(labelText: 'Color', hintText: 'Enter Color', controller: color),
-                LostInput(labelText: 'Extra', hintText: 'Extra Information', controller: extraInfo),
-                ]  ),
-
+                  MaterialButton(
+                      color: Colors.grey,
+                      shape: RoundedRectangleBorder(
+                          side: const BorderSide(
+                              width: 2, color: Color(0xffefd16f)),
+                          borderRadius: BorderRadius.circular(5)),
+                      child: const Text("Pick Image",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 10)),
+                      onPressed: () {
+                        pickImage();
+                      }),
+                ]),
+                SizedBox(
+                  height: 20,
                 ),
-
-
+                Container(
+                  child: Column(children: [
+                    LostInput(
+                        labelText: 'Type',
+                        hintText: "Enter  Name ",
+                        controller: name),
+                    LostInput(
+                        labelText: 'Serial Number',
+                        hintText: 'Enter  S.N',
+                        controller: SerialNumber),
+                    LostInput(
+                        labelText: 'Model',
+                        hintText: 'Enter  Model',
+                        controller: model),
+                    LostInput(
+                        labelText: 'Color',
+                        hintText: 'Enter Color',
+                        controller: color),
+                    LostInput(
+                        labelText: 'Extra',
+                        hintText: 'Extra Information',
+                        controller: extraInfo),
+                  ]),
+                ),
               ],
             ),
-
+            Stack(
+              children: [
+                FlutterMap(
+                  options: MapOptions(zoom: 10.0, center: LatLng(33, 37)),
+                  layers: [
+                    TileLayerOptions(
+                      urlTemplate:
+                          "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                      subdomains: ['a', 'b', 'c'],
+                    ),
+                    MarkerLayerOptions(markers: [
+                      Marker(
+                          point: LatLng(33, 37),
+                          builder: (ctx) => const Icon(
+                                Icons.location_on,
+                                color: Colors.red,
+                              ),
+                          width: 100,
+                          height: 100)
+                    ])
+                  ],
+                )
+              ],
+            )
           ],
-        ),]
-      ),
+        ),
+      ]),
     );
   }
 
   Future pickImage() async {
     try {
       final image = await ImagePicker().pickImage(source: ImageSource.gallery);
-      if(image == null) return;
+      if (image == null) return;
       final imageTemp = File(image.path);
       setState(() => this.image = imageTemp);
-    } on PlatformException catch(e) {
+    } on PlatformException catch (e) {
       print('Failed to pick image: $e');
     }
   }
+
+  
 }
