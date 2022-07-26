991c0d7371a5582a8a3dd6f952bacd69f3ec1cbd
NoureddineAbouZahr <naz.199860@gmail.com>
2022-07-26 22:01:48 +0300

    displaying item image in  item data

--- mobileFrontend/lib/pages/itemData.dart
+++ mobileFrontend/lib/pages/itemData.dart
@@ -1,30 +1,54 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:lost/pages/speclost.dart';
 
-    class LItem extends StatelessWidget {
+class LItem extends StatelessWidget {
   final Post post;
 
-      const LItem({Key? key,required this.post}) : super(key: key);
+  const LItem({Key? key, required this.post}) : super(key: key);
 
-      @override
-      Widget build(BuildContext context) {
-        return Scaffold(
-          appBar: PreferredSize(
-            preferredSize: Size.fromHeight(60),
-            child: AppBar(
-              centerTitle: true,
-              title: Column(
-                children: const [
-                  SizedBox(height: 5),
-                  CircleAvatar(
-                    backgroundImage: AssetImage('assets/logo.png'),
-                    radius: 40,
-                    backgroundColor: Colors.transparent,
-                  )
-                ],
-              ),
-            ),
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: AppBar(
+          centerTitle: true,
+          title: Column(
+            children: const [
+              SizedBox(height: 5),
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/logo.png'),
+                radius: 40,
+                backgroundColor: Colors.transparent,
+              )
+            ],
           ),
-        );
-      }
-    }
+        ),
+      ),
+      body:
+      Center(
+        
+        child:
+      Column(
+
+        children: [
+          SizedBox(
+              width: width*0.95,
+              height: 300,
+              child:
+
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: Image.memory(base64Decode(post.img64)).image)),
+          ))
+        ],
+      ),
+    ));
+  }
+}
