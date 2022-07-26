ff67370a3251e14f8d838649b56812ba3baf752a
NoureddineAbouZahr <naz.199860@gmail.com>
2022-07-26 22:22:53 +0300

    item title

--- mobileFrontend/lib/pages/itemData.dart
+++ mobileFrontend/lib/pages/itemData.dart
@@ -13,42 +13,56 @@ class LItem extends StatelessWidget {
     double width = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60),
-        child: AppBar(
-          centerTitle: true,
-          title: Column(
-            children: const [
-              SizedBox(height: 5),
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/logo.png'),
-                radius: 40,
-                backgroundColor: Colors.transparent,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(60),
+          child: AppBar(
+            centerTitle: true,
+            title: Column(
+              children: const [
+                SizedBox(height: 5),
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/logo.png'),
+                  radius: 40,
+                  backgroundColor: Colors.transparent,
+                )
+              ],
+            ),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                  width: width ,
+                  height: 300,
+                  child: Container(
+                    margin: EdgeInsets.only(top:3 ),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(5)),
+                        border: Border.all(color: Color(0xffefd16f), width: 3),
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image:
+                                Image.memory(base64Decode(post.img64)).image)),
+                  )),
+              Container(
+                child: Center(
+                  child: Text(post.name,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+
+                  ),),
+                ),
+
+              ),
+              Row(
+                children: [
+                  
+                ],
               )
             ],
           ),
-        ),
-      ),
-      body:
-      Center(
-        
-        child:
-      Column(
-
-        children: [
-          SizedBox(
-              width: width*0.95,
-              height: 300,
-              child:
-
-          Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: Image.memory(base64Decode(post.img64)).image)),
-          ))
-        ],
-      ),
-    ));
+        ));
   }
 }
